b274c19424a89bae3b60370664c199f2a27594d4
Iris19383 <iris19383@outlook.com>
2024-06-16 17:43:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function fadeIn() {
+  var fadeText = document.getElementById('fade-text');
+  fadeText.style.opacity = '1'; // 將透明度設置為 1，實現淡入效果
+}
+
+function fadeOut() {
+  var fadeText = document.getElementById('fade-text');
+  fadeText.style.opacity = '0'; // 將透明度設置為 0，實現淡出效果
+}
